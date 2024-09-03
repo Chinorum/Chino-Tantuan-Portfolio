e3b95e41c85ab44ab7aedf241a1f8ebd2d144488
Chino <philiptantuan@gmail.com>
2024-09-04 04:53:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contactBtn").onclick = function() {
+    window.open(
+        "contact.html",
+        "Contact Me",
+        "width=400,height=400,left=100,top=100"
+    );
+};
+
